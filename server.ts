046624c5a49bc85223b7769cea82f68bc1a1aee5
Jeonghyeon <fishbox0923@gmail.com>
2025-01-24 14:46:02 +0000

    Redis Pub/Sub logic implemented

--- server.ts
+++ server.ts
@@ -20,32 +20,6 @@ const io = new Server(server, {
 instrument(io, {
   auth: false
 });
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-  socket.on('join_room', (roomName, userName) => {
-    socket.join(roomName);
-    socket.to(roomName).emit(`welcome ${userName}`);
-  });
-  socket.on ('offer', (offer, roomName) => {
-    socket.to(roomName).emit('offer', offer);
-
-  });
-  socket.on ('answer', (answer, roomName) => {
-    socket.to(roomName).emit('answer', answer);
-  });
-  socket.on('ice', (ice, roomName) => {
-    socket.to(roomName).emit('ice', ice);
-   });
-});
-
-server.listen(3000, () => {
-  console.log('Socket IO server listening on port 3000');
-});
 
 // Redis 연결
 const redisClient = createClient({
@@ -55,18 +29,48 @@ const redisClient = createClient({
   }
 });
 
-redisClient.connect()
+io.on('connection', (socket) => {
+  socket.on('join_room', (roomName, userName) => {
+    // 클라이언트를 특정 방에 참가시킴
+    socket.join(roomName);
+    // 해당 방에 있는 다른 클라이언트들에게 환영 메시지 전송
+    socket.to(roomName).emit('welcome', `${userName} has joined the room`);
+  });
+
+  socket.on('offer', (offer, roomName) => {
+    socket.to(roomName).emit('offer', offer);
+  });
+
+  socket.on('answer', (answer, roomName) => {
+    socket.to(roomName).emit('answer', answer);
+  });
+
+  socket.on('ice', (ice, roomName) => {
+    socket.to(roomName).emit('ice', ice);
+  });
+});
+
+redisClient
+  .connect()
   .then(() => {
     console.log('Connected to Redis');
-    // 패턴 기반 구독 (room:*)
+
     redisClient.pSubscribe('room:*', (message, channel) => {
-      console.log(message);
+      console.log(`Received message: ${message} on channel: ${channel}`);
 
       const roomName = channel.split(':')[1];
       const userName = message;
 
-      //TODO: 실제로 생성이 되었는지 확인을 해봐야함
-      io.emit('join_room', roomName, userName); 
+      // 특정 방의 존재 여부 확인 후 생성/참가
+      const socketsInRoom = io.sockets.adapter.rooms.get(roomName);
+
+      if (!socketsInRoom) {
+        // 방이 존재하지 않으면 새로 생성
+        io.emit('join_room', roomName, userName); // 방 생성 후 참가
+      } else {
+        // 방이 이미 존재하면 참가
+        io.to(roomName).emit('welcome', `${userName} has joined the room`);
+      }
     });
   })
   .catch((err) => {
